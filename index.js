80e180349ce6703f3cd06e080c513d68a33928b4
Calvin Frank <Calvin Anderson Frank>
2022-05-30 09:55:35 -0400

    sounds!      next is the border

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("hi boiiiiiiii")
+window.addEventListener("keydown", function(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    if(!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+});
+
+
